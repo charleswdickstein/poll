ea2747ee8c09b2e819bb17d21206563e33c349ef
Charles Dickstein <charleswdickstein@gmail.com>
2016-04-10 16:14:06 -0400

    blah

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ switch ($voteValue){
 
 
 
-$theDecision = array($canditateNameValue => $voteValue);
+$theDecision = array($canditateNameValue => $voteValue, $nameValue);
 // $trump = $_POST['yourvote']."\n";
 // $sanders = $_POST['yourvote']."\n";
 // $cruz = $_POST['yourvote']."\n";
