3de02ce636107c7ff7c3b70847ec7e457662a067
Charles Dickstein <charleswdickstein@gmail.com>
2016-04-10 17:12:11 -0400

    blAH

--- store_info_big_read.php
+++ store_info_big_read.php
@@ -33,16 +33,16 @@ error_reporting(E_ALL);
 			//echo $thefile[$line][$key];
 		
 		//echo "This is the print statement".$thefile[$line];
-		if ($thefile[$line] == 1){
+		if ($thefile[$line][0] == 1){
 			$clintonCount++;
 		}
-		else if ($thefile[$line] == 2){
+		else if ($thefile[$line][0] == 2){
 			$trumpCount++;
 		}
-		else if ($thefile[$line] == 3){
+		else if ($thefile[$line][0] == 3){
 			$sandersCount++;
 		}
-		else if ($thefile[$line] == 4){
+		else if ($thefile[$line][0] == 4){
 			$cruzCount++;
 		}
 		
