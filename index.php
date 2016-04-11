22748ed976847d70995439fd200fa31a3abb932f
Charles Dickstein <charleswdickstein@gmail.com>
2016-04-10 23:03:28 -0400

    blah

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ $theDecision = array($canditateNameValue => $voteValue, $nameValue);
 
 </ul>
 <?php
-	$myfile = fopen('myinfo.csv', 'a');
+	$myfile = fopen('myinfo.csv', 'r');
 	$hasVoted = -1;
 	while ($row= fgetcsv($myfile, 1024, ",")){
 
