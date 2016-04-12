69bd74a329c8fbfbf47539fb0bf3199279749066
Charles Dickstein <charleswdickstein@gmail.com>
2016-04-12 14:10:24 -0400

    blah

--- handle_form.php
+++ handle_form.php
@@ -42,7 +42,19 @@ $theDecision = array($canditateNameValue => $voteValue, $nameValue);
 </ul>
 
 <?php
-	// $fp = fopen('myinfo.csv',"r") or die("can't open the file!");
+	$handle = fopen('myinfo.csv',"r") or die("can't open the file!");
+	$row = 1;
+if (($handle = fopen("test.csv", "r")) !== FALSE) {
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $num = count($data);
+        echo "<p> $num fields in line $row: <br /></p>\n";
+        $row++;
+        for ($c=0; $c < $num; $c++) {
+            echo $data[$c] . "<br />\n";
+        }
+    }
+    fclose($handle);
+}
 	
 	// print("<table border='1' cellspacing='2' cellpadding='2'>\n");
 	// //$myfile = fopen('myinfo.csv', 'a');
